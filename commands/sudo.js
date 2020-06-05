41b9cb0cb21f57dfe8f9a4e0ca4a266b65c236aa
Willy <wsquarepa@gmail.com>
2020-06-05 08:22:48 -0700

    Added set sudo command, also added everyone's beta stuffs

--- commands/sudo.js
+++ commands/sudo.js
@@ -7,15 +7,17 @@ module.exports = {
     name: 'sudo',
 	description: 'Super User - Bot admins only.',
     args: true,
-    usage: '<command> [command args | null]',
+    usage: '<command> <@mention> [Rest of the command args]',
     guildOnly: false,
     aliases: ['su', 'bot'],
     cooldown: 0,
 	execute(message, args) {
-        if (!config["bot-admins"].includes(message.author.id)) return message.channel.send("You can't run that.")
+        if (!config["bot-admins"].includes(message.author.id) || 
+            userData[message.author.id].account.type.toLowerCase() != "admin") return message.channel.send("You can't run that.")
+
+        var mention = message.mentions.users.first()
 
         if (args[0] == "addMoney") {
-            var mention = message.mentions.users.first()
             try {
                 userData[mention.id][args[1]] += parseInt(args[3])
                 message.channel.send("Complete! Added $" + args[3])
@@ -23,7 +25,6 @@ module.exports = {
                 message.channel.send("Error, Something went wrong.")
             }
         } else if (args[0] == "removeMoney") {
-            var mention = message.mentions.users.first()
             try {
                 userData[mention.id][args[1]] -= parseInt(args[3])
                 message.channel.send("Complete! Removed $" + args[3])
@@ -31,13 +32,14 @@ module.exports = {
                 message.channel.send("Error, Something went wrong.")
             }
         } else if (args[0] == "botBan") {
-            var mention = message.mentions.users.first()
             userData[mention.id].account.type = "banned"
             message.channel.send("Complete! " + mention.username + " was banned!")
         } else if (args[0] == "botUnban") {
-            var mention = message.mentions.users.first()
             userData[mention.id].account.type = "user"
             message.channel.send("Complete! " + mention.username + " was unbanned!")
+        } else if (args[0] == "set") {
+            userData[mention.id].account.type = args.splice(2).join(" ")
+            message.channel.send("Complete! " + mention.username + " is now a " + args.splice(2).join(" ") + "!")
         } else {
             message.channel.send("That's not a valid command.")
         }
