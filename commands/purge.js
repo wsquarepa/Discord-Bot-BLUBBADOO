91a744d1f6c31edd0fb48320d2a4b5b2577ae651
Willy <wsquarepa@gmail.com>
2020-06-05 17:27:01 -0700

    fixed the purge bug

--- commands/purge.js
+++ commands/purge.js
@@ -11,23 +11,26 @@ module.exports = {
     aliases: ['clear', 'delete'],
     cooldown: 2.3,
 	execute(message, args, mention) {
-        try {
-            args[0] = parseInt(args[0])
 
-            if (args[0].toString() == "NaN") {
+        message.delete().then(function() {
+            try {
+                args[0] = parseInt(args[0])
+    
+                if (args[0].toString() == "NaN") {
+                    args[0] = 100
+                }
+            } catch {
                 args[0] = 100
             }
-        } catch {
-            args[0] = 100
-        }
-
-        console.log(args[0])
-
-        message.channel.bulkDelete(args[0], true).catch(function(error) {
-            console.error(error)
-            message.channel.send("Uh oh, it seems like I don't have permissions to do that!")
-        }).then(function(messages) {
-            message.channel.send(messages.size + " messages deleted!").then(m => m.delete({timeout: 2000}))
+    
+            console.log(args[0])
+    
+            message.channel.bulkDelete(args[0], true).catch(function(error) {
+                console.error(error)
+                message.channel.send("Uh oh, it seems like I don't have permissions to do that!")
+            }).then(function(messages) {
+                message.channel.send(messages.size + " message(s) deleted!").then(m => m.delete({timeout: 2000}))
+            })
         })
     }
 }
