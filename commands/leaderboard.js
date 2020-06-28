3357028d2738ef0770cb13b528cdca2bd59c66c8
Willy <wsquarepa@gmail.com>
2020-06-28 09:34:55 -0700

    created blue text for leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -17,45 +17,48 @@ module.exports = {
     usage: '',
     guildOnly: false,
     aliases: ['leader', 'board'],
-    cooldown: 7.5,
+    cooldown: 5,
 	execute(message, args, mention) {
         var leaders = []
         var keys = Object.keys(userData)
         var dict = {}
 
-        Object.assign(dict, userData)
+        message.channel.send("Creating leaderboard... please wait.").then(function(msg) {
+            Object.assign(dict, userData)
 
-        for (var i = 0; i < keys.length; i++) {
-            if (dict[keys[i]].account.type.toLowerCase() == "admin" || dict[keys[i]].account.type.toLowerCase() == "banned") {
-                delete dict[keys[i]]
+            for (var i = 0; i < keys.length; i++) {
+                if (dict[keys[i]].account.type.toLowerCase() == "admin" || dict[keys[i]].account.type.toLowerCase() == "banned") {
+                    delete dict[keys[i]]
+                }
             }
-        }
+    
+            // Create items array
+            var items = Object.keys(dict).map(function (key) {
+                return [dict[key].username, dict[key].cash];
+            });
+    
+            items.sort(function (first, second) {
+                return second[1] - first[1];
+            });
+    
+            var userLocation = items.findIndex((x) => x[0] == message.author.username) + 1
+    
+            var footer = "You are #" + userLocation + " of " + keys.length + " users."
+    
+            if (userLocation == 0) {
+                footer = "You are a bot ADMIN, you do not show on the leaderboard."
+            }
+            
+            leaders = items.slice(0, 5);
+            
+            var leaderString = ""
+    
+            for (var i = 0; i < leaders.length; i++) {
+                leaderString += leaders[i][0] + " - [$" + leaders[i][1] + "](" + msg.url + ")\n"
+            }
 
-        // Create items array
-        var items = Object.keys(dict).map(function (key) {
-            return [dict[key].username, dict[key].cash];
-        });
-
-        items.sort(function (first, second) {
-            return second[1] - first[1];
-        });
-
-        var userLocation = items.findIndex((x) => x[0] == message.author.username) + 1
-
-        var footer = "You are #" + userLocation + " of " + keys.length + " users."
-
-        if (userLocation == 0) {
-            footer = "You are a bot ADMIN, you do not show on the leaderboard."
-        }
-        
-        leaders = items.slice(0, 5);
-
-        var leaderString = ""
-
-        for (var i = 0; i < leaders.length; i++) {
-            leaderString += leaders[i][0] + " - $" + leaders[i][1] + "\n"
-        }
-
-        message.channel.send(embed("THE WORLD'S LEADERS: FIRST 5", leaderString, "fffffa").setFooter(footer))
+            msg.edit("", embed("THE WORLD'S LEADERS: FIRST 5", leaderString, "fffffa").setFooter(footer))
+            
+        })
     }
 }
