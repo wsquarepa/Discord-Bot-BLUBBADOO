2008d790e3267235ed6da55ade59e7a5f916000f
Willy <wsquarepa@gmail.com>
2020-07-06 17:46:17 -0700

    fixed a bug

--- commands/inventory.js
+++ commands/inventory.js
@@ -1,6 +1,7 @@
 const userData = require('../userData.json')
 const discord = require('discord.js')
 const shopData = require('../shop.json')
+const specialShopData = require('../specialShop.json')
 
 function emoji(id, message) {
     return message.client.emojis.cache.find(x => x.id == id).toString()
@@ -28,7 +29,11 @@ module.exports = {
                 for (var i = 0; i < keys.length; i++) {
                     if (!(parseInt(userInv[keys[i]].amount) < 1)) {
                         var usesDisplay =  "- " + userInv[keys[i]].uses + " use(s) for current item left."
-                        itemString += (shopData[keys[i]].image.length > 5? emoji(shopData[keys[i]].image, message):shopData[keys[i]].image) 
+                        var item = shopData[keys[i]]
+                        if (!item) {
+                            item = specialShopData[keys[i]]
+                        }
+                        itemString += (item.image.length > 5? emoji(item.image, message):item.image) 
                             + " " + userInv[keys[i]].amount + " " + keys[i] + "(s) " + (userInv[keys[i]].uses == 1? "":usesDisplay) + "\n \n"
                     }
                 }
