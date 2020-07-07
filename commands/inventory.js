3428af28ab4355a8d200b30f0099f434cda24717
Willy <wsquarepa@gmail.com>
2020-07-06 17:43:40 -0700

    added viewing of other's inventories

--- commands/inventory.js
+++ commands/inventory.js
@@ -15,30 +15,64 @@ module.exports = {
     aliases: ['inv'],
     cooldown: 3,
 	execute(message, args, mention) {
-        var userInv = userData[message.author.id].inventory
-        var keys = Object.keys(userInv)
-        var embed = new discord.MessageEmbed()
+        if (!mention) {
+            var userInv = userData[message.author.id].inventory
+            var keys = Object.keys(userInv)
+            var embed = new discord.MessageEmbed()
 
-        if (keys.toString() == "[]") {
-            embed.setTitle("Your inventory").setDescription("You have nothing!").setColor("ff0000")
-            message.channel.send(embed)
-        } else {
-            var itemString = ""
-            for (var i = 0; i < keys.length; i++) {
-                if (!(parseInt(userInv[keys[i]].amount) < 1)) {
-                    var usesDisplay =  "- " + userInv[keys[i]].uses + " use(s) for current item left."
-                    itemString += (shopData[keys[i]].image.length > 5? emoji(shopData[keys[i]].image, message):shopData[keys[i]].image) 
-                        + " " + userInv[keys[i]].amount + " " + keys[i] + "(s) " + (userInv[keys[i]].uses == 1? "":usesDisplay) + "\n \n"
-                }
-            }
-
-            if (itemString == "") {
+            if (keys.toString() == "[]") {
                 embed.setTitle("Your inventory").setDescription("You have nothing!").setColor("ff0000")
                 message.channel.send(embed)
-                return
+            } else {
+                var itemString = ""
+                for (var i = 0; i < keys.length; i++) {
+                    if (!(parseInt(userInv[keys[i]].amount) < 1)) {
+                        var usesDisplay =  "- " + userInv[keys[i]].uses + " use(s) for current item left."
+                        itemString += (shopData[keys[i]].image.length > 5? emoji(shopData[keys[i]].image, message):shopData[keys[i]].image) 
+                            + " " + userInv[keys[i]].amount + " " + keys[i] + "(s) " + (userInv[keys[i]].uses == 1? "":usesDisplay) + "\n \n"
+                    }
+                }
+
+                if (itemString == "") {
+                    embed.setTitle("Your inventory").setDescription("You have nothing!").setColor("ff0000")
+                    message.channel.send(embed)
+                    return
+                }
+                embed.setTitle("Your inventory").setDescription(itemString).setColor("00ff00")
+                message.channel.send(embed)
             }
-            embed.setTitle("Your inventory").setDescription(itemString).setColor("00ff00")
-            message.channel.send(embed)
+        } else {
+            if (!userData[mention.id]) {
+                message.channel.send("That person doesn't have a bank account yet!")
+                return false;
+            }
+
+            var userInv = userData[mention.id].inventory
+            var keys = Object.keys(userInv)
+            var embed = new discord.MessageEmbed()
+
+            if (keys.toString() == "[]") {
+                embed.setTitle("Your inventory").setDescription("You have nothing!").setColor("ff0000")
+                message.channel.send(embed)
+            } else {
+                var itemString = ""
+                for (var i = 0; i < keys.length; i++) {
+                    if (!(parseInt(userInv[keys[i]].amount) < 1)) {
+                        var usesDisplay =  "- " + userInv[keys[i]].uses + " use(s) for current item left."
+                        itemString += (shopData[keys[i]].image.length > 5? emoji(shopData[keys[i]].image, message):shopData[keys[i]].image) 
+                            + " " + userInv[keys[i]].amount + " " + keys[i] + "(s) " + (userInv[keys[i]].uses == 1? "":usesDisplay) + "\n \n"
+                    }
+                }
+
+                if (itemString == "") {
+                    embed.setTitle("Your inventory").setDescription("You have nothing!").setColor("ff0000")
+                    message.channel.send(embed)
+                    return
+                }
+                embed.setTitle("Your inventory").setDescription(itemString).setColor("00ff00")
+                message.channel.send(embed)
+            }
         }
+        
     }
 }
