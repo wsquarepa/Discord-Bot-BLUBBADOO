02c40a9f0077b1d399a79b936870a548b895d2c3
Willy <wsquarepa@gmail.com>
2020-07-24 16:54:00 -0700

    fixed erroring

--- index.js
+++ index.js
@@ -353,7 +353,7 @@ client.on('message', message => {
 			setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 		}
 	} catch (error) {
-		if (modeOfUser.testMode) {
+		if (modeOfUser.testMode && message.guild.id != "264445053596991498") {
 			console.error(error);
 		}
 		message.reply('There was an error trying to execute that command!').catch()
