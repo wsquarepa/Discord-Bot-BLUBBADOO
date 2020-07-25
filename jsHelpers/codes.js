0844e67571fcf2bb5db135a080a3977b2bde1879
Willy <wsquarepa@gmail.com>
2020-07-24 18:26:22 -0700

    added new code

--- jsHelpers/codes.js
+++ jsHelpers/codes.js
@@ -4,5 +4,12 @@ module.exports = {
         gems: 0,
         item: "",
         title: "Cow"
+    },
+
+    pAcIfIc_AcAdEmY:{
+        money: 1000,
+        gems:2,
+        item: "",
+        title: "PAEERS"
     }
 }
