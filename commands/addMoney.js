d683611a30d6d63e72b2efcb5be099f0c4870ff0
wsquarepa <wsquarepa@gmail.com>
2020-08-25 08:30:14 -0700

    added addmoney and removemoney commands

---
+++ commands/addMoney.js
@@ -0,0 +1,42 @@
+var userData = require('../userData.json')
+const fs = require('fs');
+const discord = require("discord.js")
+
+module.exports = {
+    name: 'addMoney',
+	description: 'Add money to a user',
+    args: true,
+    usage: '<@mention> <cash|bank> <amount>',
+    guildOnly: false,
+    aliases: [],
+    cooldown: 0,
+    levelRequirement: 0,
+    category: "economy",
+    adminOnly: true,
+	execute(message, args, mention) {
+        const amount = parseInt(args[3])
+        if (!amount) {
+            message.channel.send("Enter a valid amount!")
+            return;
+        }
+
+        if (!mention) {
+            message.channel.send("Mention someone!")
+            return;
+        }
+
+        if (!args[1]) {
+            message.channel.send("Enter a valid type!")
+            return;
+        }
+
+        try {
+            userData[mention.id][args[1]] += amount
+            fs.writeFile("./userData.json", JSON.stringify(userData), (err) => err !== null ? console.error(err) : null)
+            message.channel.send("Complete! Added " + args[3] + " " + args[1] + "(s) to user.\n Current user status: ```json\n" + JSON.stringify(userData[mention.id]) + "\n```")
+        } catch(e) {
+            message.channel.send("Error, Something went wrong.")
+            console.error(e)
+        }
+    }
+}
