d40fea0257bbdf93fe66dca675a3853a024c137e
wsquarepa <wsquarepa@gmail.com>
2020-08-26 16:06:24 -0700

    fixing inv bug

--- commands/inventory.js
+++ commands/inventory.js
@@ -9,7 +9,7 @@ function emoji(id, message) {
 
 module.exports = {
     name: 'inventory',
-	description: 'Check your inventory!',
+    description: 'Check your inventory!',
     args: false,
     usage: '',
     guildOnly: false,
@@ -17,20 +17,24 @@ module.exports = {
     cooldown: 3,
     category: "economy",
     adminOnly: false,
-	execute(message, args, mention) {
+    execute(message, args, mention) {
         if (!mention) {
             var userInv = userData[message.author.id].inventory
             var pages = []
             var keys = Object.keys(userInv)
-
-            for (var i = 0; i < (keys.length / 5); i++) {
+            var keysLength = keys.length
+            for (var i = 0; i < (keysLength / 5); i++) {
                 try {
                     pages.push(keys.splice(0, 5))
                 } catch {
                     //pass
                 }
             }
-            var page = (args[0] == null? 0:parseInt(args[0]) - 1)
+
+            console.log(pages)
+
+            var page = (args[0] == null ? 0 : parseInt(args[0]) - 1)
+            console.log(page)
             keys = pages[page]
             if (!keys) {
                 message.channel.send("That page doesn't exist.")
@@ -46,20 +50,22 @@ module.exports = {
                 var itemString = ""
                 for (var i = 0; i < keys.length; i++) {
                     if (!(parseInt(userInv[keys[i]].amount) < 1)) {
-                        var usesDisplay =  "- " + userInv[keys[i]].uses + " use(s) for current item left."
+                        var usesDisplay = "- " + userInv[keys[i]].uses + " use(s) for current item left."
                         var item = shopData[keys[i]]
                         if (!item) {
                             item = specialShopData[keys[i]]
                         }
 
                         if (!item) {
-                            item = {image: "?"}
+                            item = {
+                                image: "?"
+                            }
                         }
 
                         if (!item.image) item.image = "?"
 
-                        itemString += (item.image.length > 5? emoji(item.image, message):item.image) 
-                            + " " + userInv[keys[i]].amount + " " + keys[i] + "(s) " + (userInv[keys[i]].uses == 1? "":usesDisplay) + "\n \n"
+                        itemString += (item.image.length > 5 ? emoji(item.image, message) : item.image) +
+                            " " + userInv[keys[i]].amount + " " + keys[i] + "(s) " + (userInv[keys[i]].uses == 1 ? "" : usesDisplay) + "\n \n"
                     }
                 }
 
@@ -69,7 +75,7 @@ module.exports = {
                     return
                 }
                 embed.setTitle("Your inventory").setDescription(itemString).setColor("2f3237")
-                embed.setFooter("Shop page #" + (page + 1) + " out of " + pages.length + " pages.")
+                embed.setFooter("Inventory page #" + (page + 1) + " out of " + pages.length + " pages.")
                 message.channel.send(embed)
             }
         } else {
@@ -89,20 +95,22 @@ module.exports = {
                 var itemString = ""
                 for (var i = 0; i < keys.length; i++) {
                     if (!(parseInt(userInv[keys[i]].amount) < 1)) {
-                        var usesDisplay =  "- " + userInv[keys[i]].uses + " use(s) for current item left."
+                        var usesDisplay = "- " + userInv[keys[i]].uses + " use(s) for current item left."
                         var item = shopData[keys[i]]
                         if (!item) {
                             item = specialShopData[keys[i]]
                         }
 
                         if (!item) {
-                            item = {image: "?"}
+                            item = {
+                                image: "?"
+                            }
                         }
 
                         if (!item.image) item.image = "?"
-                        
-                        itemString += (item.image.length > 5? emoji(item.image, message):item.image) 
-                            + " " + userInv[keys[i]].amount + " " + keys[i] + "(s) " + (userInv[keys[i]].uses == 1? "":usesDisplay) + "\n \n"
+
+                        itemString += (item.image.length > 5 ? emoji(item.image, message) : item.image) +
+                            " " + userInv[keys[i]].amount + " " + keys[i] + "(s) " + (userInv[keys[i]].uses == 1 ? "" : usesDisplay) + "\n \n"
                     }
                 }
 
@@ -115,6 +123,6 @@ module.exports = {
                 message.channel.send(embed)
             }
         }
-        
+
     }
 }
