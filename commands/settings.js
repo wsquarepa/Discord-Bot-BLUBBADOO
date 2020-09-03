81057135e70f72c12c734899ec7d964ca98a004c
wsquarepa <wsquarepa@gmail.com>
2020-09-03 08:47:54 -0700

    added prefix changing command; still working on the rest of it.

--- commands/settings.js
+++ commands/settings.js
@@ -15,7 +15,7 @@ module.exports = {
     category: "moderation",
     adminOnly: false,
 	execute(message, args, mention) {
-        if (!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) {
+        if (!message.guild.member(message.author).hasPermission("MANAGE_GUILD") && args.length > 0) {
             message.channel.send("You can't do that, buddy!")
             return
         }
@@ -32,6 +32,14 @@ module.exports = {
             embed.addField("Achivement earned message: ", guild.settings.achivementMessage? "ON":"OFF")
             embed.setColor(functions.globalEmbedColor)
             message.channel.send(embed)
+        } else if (args[0] == "prefix") {
+            if (!args[1]) {
+                message.channel.send("You gotta supply a prefix; it can't be undefined.")
+                return
+            }
+
+            guildData[message.guild.id].prefix = args.slice(1).join(" ")
+            message.channel.send("Complete! Server prefix set to `" + args.slice(1).join(" ") + "`!")
         }
     }
 }
