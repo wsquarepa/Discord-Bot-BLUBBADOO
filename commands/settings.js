059a91672271de5ee397b380dabd0031acab858f
wsquarepa <wsquarepa@gmail.com>
2020-09-03 10:48:05 -0700

    added the rest of the stuff for settings

--- commands/settings.js
+++ commands/settings.js
@@ -31,6 +31,7 @@ module.exports = {
             embed.addField("Race completion DM message: ", guild.settings.raceCompletionMessage? "ON":"OFF")
             embed.addField("Achivement earned message: ", guild.settings.achivementMessage? "ON":"OFF")
             embed.setColor(functions.globalEmbedColor)
+            embed.setFooter("==settings help - for help")
             message.channel.send(embed)
         } else if (args[0] == "prefix") {
             if (!args[1]) {
@@ -40,6 +41,55 @@ module.exports = {
 
             guildData[message.guild.id].prefix = args.slice(1).join(" ")
             message.channel.send("Complete! Server prefix set to `" + args.slice(1).join(" ") + "`!")
+        } else if (args[0] == "messages") {
+            if (args[1] == "levelup") {
+                if (!args[2] || (args[2] != "on" && args[2] != "off")) {
+                    message.channel.send("You have to choose `on` or `off`")
+                }
+
+                if (args[2] == "on") {
+                    guildData[message.guild.id].settings.levelUpMessages = true
+                } else {
+                    guildData[message.guild.id].settings.levelUpMessages = false
+                }
+            } else if (args[1] == "moneyexceed") {
+                if (!args[2] || (args[2] != "on" && args[2] != "off")) {
+                    message.channel.send("You have to choose `on` or `off`")
+                }
+
+                if (args[2] == "on") {
+                    guildData[message.guild.id].settings.moneyExceedMessage = true
+                } else {
+                    guildData[message.guild.id].settings.moneyExceedMessage = false
+                }
+            } else if (args[1] == "racecompletion") {
+                if (!args[2] || (args[2] != "on" && args[2] != "off")) {
+                    message.channel.send("You have to choose `on` or `off`")
+                }
+
+                if (args[2] == "on") {
+                    guildData[message.guild.id].settings.raceCompletionMessage = true
+                } else {
+                    guildData[message.guild.id].settings.raceCompletionMessage = false
+                }
+            } else if (args[1] == "achivements") {
+                if (!args[2] || (args[2] != "on" && args[2] != "off")) {
+                    message.channel.send("You have to choose `on` or `off`")
+                }
+
+                if (args[2] == "on") {
+                    guildData[message.guild.id].settings.achivementMessage = true
+                } else {
+                    guildData[message.guild.id].settings.achivementMessage = false
+                }
+            } else {
+                message.channel.send("Could not find setting.")
+            }
+        } else if (args[0] == "help") {
+            message.channel.send("**SETTINGS HELP** \n" + 
+            "*==settings* - shows your server settings \n" + 
+            "*==settings prefix <prefix>* - set server prefix \n" + 
+            "*==settings messages <levelup | moneyexceed | racecompletion | achivements> <on | off>* - toggle messages")
         }
     }
 }
