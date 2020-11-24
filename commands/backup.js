3d4e657704e1e76395b562d1b8d85a58a20165b1
wsquarepa <wsquarepa@gmail.com>
2020-11-23 16:25:49 -0800

    creating bot data backups

--- commands/backup.js
+++ commands/backup.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 module.exports = {
     name: 'backup',
-	description: 'Create a backup',
+    description: 'Create a backup',
     args: false,
     usage: '',
     guildOnly: false,
@@ -15,27 +15,41 @@ module.exports = {
     levelRequirement: 0,
     category: "info",
     adminOnly: true,
-	execute(message, args, mention) {
+    execute(message, args, mention) {
         try {
             const today = new Date()
-            const filepath = ("./backups/userData/" + (today.getMonth() + "-" + today.getDate() + "-" + today.getFullYear() + "_" + 
-            today.getHours() + ":" + today.getMinutes()) + ".json")
-            fs.writeFile(filepath, "", (err) => {
+            const userDataFilePath = ("./backups/userData/" + (today.getMonth() + "-" + today.getDate() + "-" + today.getFullYear() + "_" +
+                today.getHours() + ":" + today.getMinutes()) + ".json")
+            fs.writeFile(userDataFilePath, "", (err) => {
                 if (err) {
                     console.error(err)
                     message.channel.send("Was not able to make backup")
                 }
 
                 var pathToFile = path.join("./userData.json")
-                var pathToNewDestination = path.join(filepath)
+                var pathToNewDestination = path.join(userDataFilePath)
                 fs.copyFileSync(pathToFile, pathToNewDestination)
-                message.channel.send("Created backup `" + filepath + "` successfully!")
+                message.channel.send("Created backup `" + userDataFilePath + "` successfully!")
+
+                const botDataFilePath = ("./backups/botData/" + (today.getMonth() + "-" + today.getDate() + "-" + today.getFullYear() + "_" +
+                    today.getHours() + ":" + today.getMinutes()) + ".json")
+                fs.writeFile(botDataFilePath, "", (err) => {
+                    if (err) {
+                        console.error(err)
+                        message.channel.send("Was not able to make backup")
+                    }
+
+                    var pathToFile = path.join("./botData.json")
+                    var pathToNewDestination = path.join(botDataFilePath)
+                    fs.copyFileSync(pathToFile, pathToNewDestination)
+                    message.channel.send("Created backup `" + botDataFilePath + "` successfully!")
+                })
             })
-			
-		} catch (error) {
+
+        } catch (error) {
             //pass
             console.error(error)
             message.channel.send("Was not able to make backup")
-		}
+        }
     }
 }
