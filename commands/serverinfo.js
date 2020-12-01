139a4b4c3f211986673f279410faa2c19c873fff
wsquarepa <wsquarepa@gmail.com>
2020-12-01 10:33:37 -0800

    fixed minor region bug

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -19,21 +19,39 @@ module.exports = {
             return;
         }
 
+        let region = {
+            "brazil": ":flag_br: Brazil",
+            "eu-central": ":flag_eu: Central Europe",
+            "singapore": ":flag_sg: Singapore",
+            "us-central": ":flag_us: U.S. Central",
+            "sydney": ":flag_au: Sydney",
+            "us-east": ":flag_us: U.S. East",
+            "us-south": ":flag_us: U.S. South",
+            "us-west": ":flag_us: U.S. West",
+            "eu-west": ":flag_eu: Western Europe",
+            "vip-us-east": ":flag_us: VIP U.S. East",
+            "london": ":flag_gb: London",
+            "amsterdam": ":flag_nl: Amsterdam",
+            "hongkong": ":flag_hk: Hong Kong",
+            "russia": ":flag_ru: Russia",
+            "southafrica": ":flag_za:  South Africa"
+        };
+
         var embed = new discord.MessageEmbed()
         embed.setTitle("Server information:")
             .addField("Name", message.guild.name, true)
             .addField("ID", message.guild.id, true)
             .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
             .addField("Region", region[message.guild.region], true)
-            .addField("Guild Size (Total | Humans | Bots)", `${message.guild.members.cache.size} | ${message.guild.members.cache.filter(member => !member.user.bot).size}` + 
-            ` | ${message.guild.members.cache.filter(member => member.user.bot).size}`, true)
+            .addField("Guild Size (Total | Humans | Bots)", `${message.guild.members.cache.size} | ${message.guild.members.cache.filter(member => !member.user.bot).size}` +
+                ` | ${message.guild.members.cache.filter(member => member.user.bot).size}`, true)
             .addField("Channels", message.guild.channels.cache.size, true)
             .addField("Roles", message.guild.roles.cache.size, true)
             .addField("Verification Level", message.guild.verificationLevel, true)
             .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
             .setThumbnail(message.guild.iconURL)
             .setColor(functions.globalEmbedColor)
-        
+
         message.channel.send(embed)
     }
 }
