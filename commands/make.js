d749e802fbc498d6d65a04c056b2053d58132d4c
wsquarepa <wsquarepa@gmail.com>
2020-12-01 10:41:20 -0800

    make is now an embed.

--- commands/make.js
+++ commands/make.js
@@ -1,6 +1,7 @@
 var userData = require('../userData.json')
 const fs = require('fs');
 const discord = require("discord.js")
+const functions = require("../jsHelpers/functions")
 
 function randomNumber(min, max) {
     min = Math.ceil(min);
@@ -20,12 +21,16 @@ module.exports = {
     category: "economy",
     adminOnly: false,
 	execute(message, args, mention) {
-        const makeables = ['video', 'cake', 'cookie', 'game', 'website', 'discord bot']
+        const makeables = ['video', 'cake', 'cookie', 'game', 'website', 'discord bot', 'gun']
         const making = makeables[randomNumber(0, makeables.length - 1)]
         const earnings = randomNumber(100, 1000) * userData[message.author.id].intellegencePoints
 
         userData[message.author.id].cash += earnings
         fs.writeFile("./userData.json", JSON.stringify(userData), (err) => err !== null ? console.error(err) : null)
-        message.channel.send("You made a " + making + " and sold it for $" + earnings + "!")
+        var embed = new discord.MessageEmbed()
+        embed.setTitle("Make Success!")
+        embed.setDescription("You made a " + making + " and sold it for $" + earnings + "!")
+        embed.setColor(functions.globalEmbedColor)
+        message.channel.send(embed)
     }
 }
