a6023c65170805616da0ae2f69cb2203b2ac977f
wsquarepa <wsquarepa@gmail.com>
2020-12-08 11:27:16 -0800

    created process exiting command

--- commands/crab.js
+++ commands/crab.js
@@ -44,9 +44,9 @@ module.exports = {
         message.channel.send("Okie, crabbing...").then(msg => {
             var chance = randomNumber(0, 3)
             const inGuild = message.channel.type == "dm" ? false : true
-            var collector = new discord.MessageCollector(message.channel, m => m.author.id == message.author.id && inGuild ? message.content.startsWith(guildData[message.guild.id].prefix)
-            && m.content !== guildData[message.guild.id].prefix + "fish" : message.content.startsWith("==") &&
-            m.content !== "==fish")
+            var collector = new discord.MessageCollector(message.channel, m => (m.author.id == message.author.id) && 
+                (inGuild ? (message.content.startsWith(guildData[message.guild.id].prefix)
+                && m.content !== guildData[message.guild.id].prefix + "fish") : (message.content.startsWith("==") && m.content !== "==fish")))
             collector.on("collect", function () {
                 msg.edit("You can't multitask.")
                 collector.stop()
