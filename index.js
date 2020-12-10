41bcd0be428df6146c04d2852f904dec7c3b841f
wsquarepa <wsquarepa@gmail.com>
2020-12-10 13:29:32 -0800

    changed debug log notification

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { ShardingManager } = require('discord.js');
 const { token } = require("./config.json")
-const manager = new ShardingManager('./bot.js', { token: token});
+const manager = new ShardingManager('./bot.js', { token: token });
 
 manager.spawn().catch(error => console.error(`[ERROR/MASTER] Shard failed to spawn.`));
 
 manager.on("shardCreate", shard => {
     // Listeing for the ready event on shard.
     shard.on("ready", () => {
-        console.log(`[DEBUG/MASTER] Shard ${shard.id} connected to Discord's Gateway.`)
+        console.log(`[DEBUG/MASTER] Shard ${shard.id} launched.`)
         // Sending the data to the shard.
         shard.send({type: "shardId", data: {shardId: shard.id}});
     });
