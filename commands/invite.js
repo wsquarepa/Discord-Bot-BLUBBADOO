48a21d9b140bdb452bb94ae22d156993c21bd445
wsquarepa <wsquarepa@gmail.com>
2020-12-15 19:31:38 -0800

    invite link changed

--- commands/invite.js
+++ commands/invite.js
@@ -1,4 +1,4 @@
-//link: https://discord.com/oauth2/authorize?&client_id=596715111511490560&scope=bot&permissions=1056435583
+//link: https://discord.com/oauth2/authorize?&client_id=596715111511490560&scope=bot&permissions=2147483647
 module.exports = {
 	name: 'invite',
 	description: 'Invite for the bot!',
@@ -11,7 +11,7 @@ module.exports = {
                 maxAge: 900
             }).then((invite) => {
                 const inviteCode = invite.code
-                message.channel.send("**Bot invite is here:** <https://discord.com/oauth2/authorize?&client_id=596715111511490560&scope=bot&permissions=1056435583> \n" + 
+                message.channel.send("**Bot invite is here:** <https://discord.com/oauth2/authorize?&client_id=596715111511490560&scope=bot&permissions=2147483647> \n" + 
                 "**Server invite is here:** https://discord.gg/" + inviteCode + "\n" +
                 "This message will try to **AUTO DESTRUCT** when the invite expires.").then(m => {
                     m.delete({timeout: invite.maxAge * 1000}).catch()
