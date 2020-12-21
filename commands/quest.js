01896c580626ce37910d30f6cd4187a1e4fb565d
wsquarepa <wsquarepa@gmail.com>
2020-12-21 12:26:59 -0800

    removed warning msg

--- commands/quest.js
+++ commands/quest.js
@@ -62,8 +62,8 @@ module.exports = {
                 return
             }
 
-            message.channel.send("Are you 100% sure you want to start the quest? When you start a quest, you **CANNOT** execute any commands except for this one, " +
-            "as well you may die and lose most of your health. Also, you must do `quest collect` to collect your reward within an hour of finishing. Otherwise, " + 
+            message.channel.send("Are you 100% sure you want to start the quest? When you start a quest, you **CANNOT** execute any commands except for this one. " +
+            "Also, you must do `quest collect` to collect your reward within an hour of finishing. Otherwise, " + 
             "you **will not** automatically collct your reward. Also, you can end your quest at any time by saying `quest end`. " + 
             "Say **yes** to confirm that you would like to start the quest.")
             const collector = new discord.MessageCollector(message.channel, x => x.author.id == message.author.id, {time: 10000, max: 1})
